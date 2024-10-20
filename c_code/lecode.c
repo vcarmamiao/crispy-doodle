f053828c99640de8e1579b42ae2fbf5bbf6f1c1f
vcarmamiao <kxusong@gmail.com>
2024-10-20 14:16:07 +0800

    ccd

--- c_code/lecode.c
+++ c_code/lecode.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
+    int cc=0;
+    scanf("%d",&cc);
     for(int i=1;i<12;i++){
         for(int j=1;j<2;j++){
-            printf("Hello World!\n");
+            printf("%d ff\n",i);
         }
     }
+    printf("%d",cc);
     return 0;
 }
